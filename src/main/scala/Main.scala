e4fdae128cb97ac0077e3af6dbf419a684ea6c04
mika <ionkinmikhail@gmail.com>
2022-08-14 16:47:23 +0300

    attempt to use random forest from smile

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 package org.ionkin.ml.test
 
 import java.nio.file.Paths
-import java.util.Random
+import smile.classification.*
+import smile.data.formula.FormulaBuilder
+import smile.read
+import smile.data.formula.FormulaBuilder.*
+import smile.data.formula.Terms
+
+import scala.collection.mutable.ListBuffer
 
 @main def main(): Unit =
   val wineCsv = Paths.get(getClass.getClassLoader.getResource("wine.data").toURI).toFile
-
-
+  val wine = read.csv(wineCsv.getAbsolutePath)
+  val formula = FormulaBuilder(Option(Terms.$("1")), ListBuffer()).toFormula
+  val model = randomForest(formula, wine)
+  println(model.metrics)
