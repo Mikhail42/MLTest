cd1319ef29512f6b7d68c7a64763f492f856ba18
mika <ionkinmikhail@gmail.com>
2022-08-19 01:04:16 +0300

    move file to package

--- src/main/scala/org/ionkin/ml/test/Main.scala
+++ src/main/scala/org/ionkin/ml/test/Main.scala
@@ -149,7 +149,7 @@ object Main extends StrictLogging {
   }
 
   def optimize[X, Y, M](x: X, y: Y, hp: Hyperparameters,
-               opt: (X, Y, Properties) => ClassificationValidations[M]): List[(Properties, Double)] = {
+                        opt: (X, Y, Properties) => ClassificationValidations[M]): List[(Properties, Double)] = {
     val bestModels = hp.grid().toArray(k => new Array[Properties](k)).map { params =>
       (params, Try(opt(x, y, params)))
     }.filter(_._2.isSuccess).map(e => (e._1, e._2.get.avg.accuracy)).sortBy(_._2)(Ordering.Double.TotalOrdering.reverse)
