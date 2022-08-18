e7648a9f1eb6902830b995c02b82151ea6bc2045
mika <ionkinmikhail@gmail.com>
2022-08-19 01:02:17 +0300

    add accuracy description for cancer dataset

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -176,7 +176,7 @@ object Main extends StrictLogging {
     // MLP accuracy=0.9339869281045754 for epochs=100, layers=ReLU(41), mini_batch=1
     // k=3, accuracy=96.81% ± 3.44 for k-NN
     // sigma=1.5, regulation=6.0, accuracy=1.0 (100%)  for SVM
-    // k=3, step=0.8, accuracy=97.65% ± 4.11 for Parzen window
+    // k=3, h=0.8, accuracy=97.65% ± 4.11 for Parzen window
   }
 
   def spam(): Unit = {
@@ -187,12 +187,17 @@ object Main extends StrictLogging {
     // optimization for MLP can be really slow (1-30 seconds per iteration)
     // k=1, accuracy=91.33% ± 1.29 for k-NN
     // sigma=0.25, regulation=9.5, epochs=2, accuracy=0.838 (83.8%) for SVM
-    // k=1, step=1.2, accuracy=91.68% ± 1.05 for Parzen window
+    // k=1, h=1.2, accuracy=91.68% ± 1.05 for Parzen window
   }
 
   def cancer(): Unit = {
     val spamPath = Paths.get(getClass.getClassLoader.getResource("breast-cancer-wisconsin.csv").toURI).toFile
     show_best(spamPath, y_column_id = -1)
+    // best RDA: does not work with NaN
+    // best k-NN: k=3, accuracy=95.86% ± 2.16
+    // best Parzen window: k=3, h=2.8, accuracy=96.59% ± 2.11
+    // best SVM: 0 accuracy (I have NaN values)
+    // best MLP: accuracy < 70%
   }
 
   def main(args: Array[String]): Unit = {
