b3703375cb14a373e0a3bd7cc7f2ab5ba1cfef22
mika <ionkinmikhail@gmail.com>
2022-08-20 23:21:40 +0300

    fix x/y parsing from dataset

--- src/main/scala/Preparator.scala
+++ src/main/scala/Preparator.scala
@@ -1,14 +1,20 @@
 package org.ionkin.ml.test
 
+import com.typesafe.scalalogging.StrictLogging
 import smile.data.DataFrame
+import smile.data.`type`.StructField
 
 import java.util.Random
 import scala.reflect.ClassTag
 import scala.util.Try
 
-object Preparator {
+object Preparator extends StrictLogging {
   def classification_extract_y(data: DataFrame, y_column_id: Int): Array[Int] = {
-    val y0: Array[Int] = data.doubleVector(y_column_id).toIntArray
+    val y_schema: StructField = data.schema().field(y_column_id)
+    val y0: Array[Int] = y_schema.`type` match {
+      case t if t.isDouble => data.doubleVector(y_column_id).toIntArray
+      case t if t.isInt => data.intVector(y_column_id).array()
+    }
     // Assume that classes are 2 and 4. Then it will be map (2->0, 4->1)
     val y_to_y_id: Map[Int, Int] = y0.toSet.toSeq.sorted.zipWithIndex.toMap
     val y = y0.map(y_to_y_id)
@@ -32,6 +38,22 @@ object Preparator {
     (x, y)
   }
 
+  private def convert_x_value(d: Object): Double = d match {
+    case d: java.lang.Double => d
+    case i: java.lang.Integer => i.toDouble
+    case l: java.lang.Long => l.toDouble
+    case f: java.lang.Float => f.toDouble
+    case c: java.lang.Character if c == '?' => Double.NaN
+    case c: java.lang.Character =>
+      val lower = Character.toLowerCase(c)
+      if (lower.isDigit) lower.getNumericValue
+      else if (lower - 'a' >= 0 && 'z' - lower >= 0) (lower - 'a').toDouble
+      else throw new RuntimeException(s"Can't convert $c to double")
+    case s: java.lang.String if s.length == 1 => convert_x_value(Character.valueOf(s.charAt(0)))
+    case s: java.lang.String if Try(s.toDouble).isSuccess => s.toDouble
+    case x => throw new RuntimeException(s"Can't convert $x to double")
+  }
+
   def extract_x(x_data: DataFrame): Array[Array[Double]] = {
     // I need to handle 2 cases:
     // 1. when there is no column scheme with double. In this case I can just fix csv and convert 1st row to doubles
@@ -42,18 +64,13 @@ object Preparator {
     val x_double_data: Array[Array[Double]] = Array.ofDim[Double](x_data.size(), x_data.ncol())
     for (k <- 0 until x_data.ncol()) {
       for (i <- 0 until x_data.size()) {
-        val d: Double = Try {
-          val converted: Double = x_data.get(i, k) match {
-            case i: java.lang.Integer => i.toDouble
-            case l: java.lang.Long => l.toDouble
-            case d: java.lang.Double => d
-            case c: java.lang.Character => (Character.toLowerCase(c) - 'a').toInt.toDouble
-            case s: java.lang.String if s.length == 1 => (Character.toLowerCase(s.charAt(0)) - 'a').toInt.toDouble
-            case _ => Double.NaN
+        x_double_data(i)(k) =
+          try convert_x_value(x_data.get(i, k))
+          catch {
+            case exc: Exception =>
+              logger.error(s"Can't convert X[$i][$k] to double", exc)
+              throw exc
           }
-          converted
-        }.getOrElse(Double.NaN)
-        x_double_data(i)(k) = d
       }
     }
     x_double_data
